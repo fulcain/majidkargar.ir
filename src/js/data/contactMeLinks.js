856a60bfe4b5b4c46a6a497beca7d15c227f84d7
Majid Kargar <majidkargar01@gmail.com>
2024-02-09 19:10:22 +0330

    Feat: Add new images

--- src/js/data/contactMeLinks.js
+++ src/js/data/contactMeLinks.js
@@ -1,26 +1,27 @@
 const contactMeLinks = [
     {
         image: "public/assets/images/github.png",
-        link: "https://github.com/fulcain"
+        link: "https://github.com/fulcain",
+    },
+    {
+        image: "public/assets/images/linkedin.png",
+        link: "https://www.linkedin.com/in/majid-kargar-994b642a6",
+    },
+    {
+        image: "public/assets/images/discord.png",
+        link: "https://discordapp.com/users/290467278540242944",
+    },
+    {
+        image: "public/assets/images/whatsapp.png",
+        link: "https://wa.me/+989170177388",
+    },
+    {
+        image: "public/assets/images/telegram.png",
+        link: "https://t.me/fulcain",
     },
     {
         image: "public/assets/images/gmail.png",
-        link:"mailto:majidkargar01@gmail.com"
+        link: "mailto:majidkargar01@gmail.com",
     },
-    {
-        name: "Discord: Fulcain#2532",
-        image: "public/assets/images/discord.png",
-        link: "https://discordapp.com/users/290467278540242944"
-    },
-    {
-        name: "Whatsapp: +989170177388",
-        image: "public/assets/images/whatsapp.png",
-        link: "https://wa.me/+989170177388"
-    },
-    {
-        name: "Telegram: @fulcain",
-        image: "public/assets/images/telegram.png",
-        link: "https://t.me/fulcain"
-    },
-]
+];
 export default contactMeLinks;
