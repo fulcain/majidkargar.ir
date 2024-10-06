3824a1e75cacab50254b4eee090477f0c3246c0d
Majid Kargar <majidkargar01@gmail.com>
2024-10-06 17:59:12 +0330

    Refactor: remove whatsapp

--- src/js/data/contactMeLinks.js
+++ src/js/data/contactMeLinks.js
@@ -1,27 +1,23 @@
 const contactMeLinks = [
-    {
-        image: "public/assets/images/github.png",
-        link: "https://github.com/fulcain",
-    },
-    {
-        image: "public/assets/images/linkedin.png",
-        link: "https://www.linkedin.com/in/majid-kargar-994b642a6",
-    },
-    {
-        image: "public/assets/images/discord.png",
-        link: "https://discordapp.com/users/290467278540242944",
-    },
-    {
-        image: "public/assets/images/whatsapp.png",
-        link: "https://wa.me/+989170177388",
-    },
-    {
-        image: "public/assets/images/telegram.png",
-        link: "https://t.me/fulcain",
-    },
-    {
-        image: "public/assets/images/gmail.png",
-        link: "mailto:majidkargar01@gmail.com",
-    },
+  {
+    image: "public/assets/images/github.png",
+    link: "https://github.com/fulcain",
+  },
+  {
+    image: "public/assets/images/linkedin.png",
+    link: "https://www.linkedin.com/in/majid-kargar-994b642a6",
+  },
+  {
+    image: "public/assets/images/discord.png",
+    link: "https://discordapp.com/users/290467278540242944",
+  },
+  {
+    image: "public/assets/images/telegram.png",
+    link: "https://t.me/fulcain",
+  },
+  {
+    image: "public/assets/images/gmail.png",
+    link: "mailto:majidkargar01@gmail.com",
+  },
 ];
 export default contactMeLinks;
