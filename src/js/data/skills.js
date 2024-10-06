9de91040d3160dda50dc5a543eef921e5c89dc54
Majid Kargar <majidkargar01@gmail.com>
2024-10-06 13:00:46 +0330

    Feat: Add MUI to skills

--- src/js/data/skills.js
+++ src/js/data/skills.js
@@ -1,44 +1,49 @@
 const skills = [
-	{
-		title: "HTML",
-		branColor: "#E34F26",
-		link: "https://developer.mozilla.org/en-US/docs/Web/HTML",
-	},
-	{
-		title: "CSS",
-		branColor: "#1572B6",
-		link: "https://developer.mozilla.org/en-US/docs/Web/CSS",
-	},
-	{
-		title: "Sass",
-		branColor: "#CC6699",
-		link: "https://sass-lang.com/documentation/",
-	},
-	{
-		title: "Tailwind",
-		branColor: "#14B8A6",
-		link: "https://tailwindcss.com/docs/installation",
-	},
-	{
-		title: "Bootstrap",
-		branColor: "#7952B3",
-		link: "https://getbootstrap.com/docs/4.1/getting-started/introduction/",
-	},
-	{
-		title: "JavaScript",
-		branColor: "#F0DB4F",
-		link: "https://javascript.info/intro?map",
-	},
-	{
-		title: "TypeScript",
-		branColor: "#007ACC",
-		link: "https://www.typescriptlang.org/docs/handbook/typescript-in-5-minutes.html",
-	},
-	{
-		title: "React",
-		branColor: "#61DAFB",
-		link: "https://react.dev/",
-	},
+  {
+    title: "HTML",
+    branColor: "#E34F26",
+    link: "https://developer.mozilla.org/en-US/docs/Web/HTML",
+  },
+  {
+    title: "CSS",
+    branColor: "#1572B6",
+    link: "https://developer.mozilla.org/en-US/docs/Web/CSS",
+  },
+  {
+    title: "Sass",
+    branColor: "#CC6699",
+    link: "https://sass-lang.com/documentation/",
+  },
+  {
+    title: "Tailwind",
+    branColor: "#14B8A6",
+    link: "https://tailwindcss.com/docs/installation",
+  },
+  {
+    title: "Bootstrap",
+    branColor: "#7952B3",
+    link: "https://getbootstrap.com/docs/4.1/getting-started/introduction/",
+  },
+  {
+    title: "JavaScript",
+    branColor: "#F0DB4F",
+    link: "https://javascript.info/intro?map",
+  },
+  {
+    title: "TypeScript",
+    branColor: "#007ACC",
+    link: "https://www.typescriptlang.org/docs/handbook/typescript-in-5-minutes.html",
+  },
+  {
+    title: "React",
+    branColor: "#61DAFB",
+    link: "https://react.dev/",
+  },
+  {
+    title: "Material UI",
+    branColor: "#2196f3",
+    link: "https://mui.com",
+  },
 ];
 
 export default skills;
