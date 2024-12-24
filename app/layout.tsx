57f1fd3892b652f11428e059e38d4d35303c3dce
Majid Kargar <majidkargar01@gmail.com>
2024-12-25 00:55:36 +0330

    Feat: Add mikhak local font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,10 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import localFont from "next/font/local";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+const mikhak = localFont({
+  src: "../style/fonts/Mikhak-VF.ttf",
+  display: "swap",
 });
 
 export const metadata: Metadata = {
@@ -23,12 +18,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="fa-IR" dir="rtl">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+    <html lang="fa-IR" dir="rtl" className={mikhak.className}>
+      <body>{children}</body>
     </html>
   );
 }
