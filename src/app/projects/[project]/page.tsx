130e70e6e9fa832ec39fd48057e098aa1e3542da
Majid Kargar <majidkargar01@gmail.com>
2025-01-03 00:11:28 +0330

    Refactor: change style of technologies

--- src/app/projects/[project]/page.tsx
+++ src/app/projects/[project]/page.tsx
@@ -52,12 +52,12 @@ export default async function Project({ params }: ProjectProps) {
                 // TODO: change style
                 <div
                   key={idx}
-                  className={`${exo_2.className} border border-palette-secondary max-w-[max-content] rounded p-2 flex flex-row align-center justify-center gap-2`}
+                  className={`${exo_2.className} border border-zinc-800  max-w-[max-content] rounded p-2 flex flex-row align-center justify-center gap-2`}
                 >
                   <span className="flex justify-center items-center text-xs md:text-sm text-white">
                     {tech.name}
                   </span>
-                  <div className="bg-palette-secondary rounded-full p-1.5">
+                  <div className="rounded-full p-1.5">
                     <Image
                       alt={tech.name}
                       src={tech.icon}
