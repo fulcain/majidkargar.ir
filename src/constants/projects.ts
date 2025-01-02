70f318069c7781ec416cca6a2bb8f93023a9f3d5
Majid Kargar <majidkargar01@gmail.com>
2025-01-02 20:42:38 +0330

    Refactor: Change projects technologies

--- src/constants/projects.ts
+++ src/constants/projects.ts
@@ -1,3 +1,24 @@
+const icons = {
+  HTML5: "/icons/html5.svg",
+  CSS3: "/icons/css3.svg",
+  SASS: "/icons/sass.svg",
+  TailwindCSS: "/icons/tailwindcss.svg",
+  Bootstrap: "/icons/bootstrap.svg",
+  JavaScript: "/icons/javascript.svg",
+  TypeScript: "/icons/typescript.svg",
+  MUI: "/icons/mui.svg",
+  ChartJS: "/icons/chartjs.svg",
+  React: "/icons/react.svg",
+  NextJS: "/icons/nextjs.svg",
+  Redux: "/icons/redux.svg",
+  SocketIO: "/icons/socketio.svg",
+  swiper: "/icons/swiper.svg",
+  vite: "/icons/vite.svg",
+  ejs: "/icons/ejs.svg",
+  radixui: "/icons/radixui.svg",
+  lucide: "/icons/lucide.svg",
+};
+
 export type ProjectType = {
   projectName: string;
   projectLiveLink: string;
@@ -5,6 +26,12 @@ export type ProjectType = {
   description: string;
   projectPath: string;
   projectType: string;
+  isPrivate: boolean;
+  hasLiveLink: boolean;
+  technologies: {
+    name: string;
+    icon: string;
+  }[];
 };
 
 export const projects: ProjectType[] = [
@@ -15,6 +42,18 @@ export const projects: ProjectType[] = [
     projectRepoLink: "https://github.com/Silverethical/gym-trainer-website",
     description: "پروژه ای برای معرفی مربی باشگاه که مربوط به کارفرما بود.",
     projectType: "special",
+    isPrivate: false,
+    hasLiveLink: true,
+    technologies: [
+      { name: "SASS", icon: icons.SASS },
+      { name: "TYPESCRIPT", icon: icons.TypeScript },
+      {
+        name: "REACT",
+        icon: icons.React,
+      },
+      { name: "SWIPER", icon: icons.swiper },
+      { name: "VITE", icon: icons.vite },
+    ],
   },
   {
     projectName: "Tonrow",
@@ -23,6 +62,14 @@ export const projects: ProjectType[] = [
     projectRepoLink: "https://tonrow.ir",
     description: `پروژه ای مربوط به شرکت برنامه نویسی وبلاین که برای کارفا درست شد. هدف پروژه مدیریت مالی و سفارشات پیک موتوری هست که درون اپلیکیشن بحث های حسابدار و استفاده از مپ و ... مورد استفاده قرار گرفته شده.`,
     projectType: "special",
+    isPrivate: true,
+    hasLiveLink: false,
+    technologies: [
+      { name: "Sass", icon: icons.SASS },
+      { name: "EJS", icon: icons.ejs },
+      { name: "CHART.JS", icon: icons.ChartJS },
+      { name: "JAVASCRIPT", icon: icons.JavaScript },
+    ],
   },
   {
     projectName: "Niro Saee",
@@ -32,6 +79,20 @@ export const projects: ProjectType[] = [
     description:
       "پروژه معرفی یک شرکت متخصص در خدمات مهندسی، پیمانکاری و مشاوره و طراحی و اجرای پروژه‌های صنعتی.",
     projectType: "special",
+    isPrivate: true,
+    hasLiveLink: true,
+    technologies: [
+      { name: "SASS", icon: icons.SASS },
+      { name: "TAILWIND", icon: icons.TailwindCSS },
+      { name: "TYPESCRIPT", icon: icons.TypeScript },
+      {
+        name: "NEXT.JS",
+        icon: icons.NextJS,
+      },
+      { name: "SWIPER", icon: icons.swiper },
+      { name: "RADIX.UI", icon: icons.radixui },
+      { name: "LUCIDE", icon: icons.lucide },
+    ],
   },
   {
     projectName: "SilverBox",
@@ -41,6 +102,12 @@ export const projects: ProjectType[] = [
     description:
       "یک کتابخانه جاوا اسکریپت هست که وظیفه ساخت مودالو آلرت هارو داره که به صورت متن باز موجود هست.",
     projectType: "special",
+    isPrivate: false,
+    hasLiveLink: true,
+    technologies: [
+      { name: "SASS", icon: icons.SASS },
+      { name: "JAVASCRIPT", icon: icons.JavaScript },
+    ],
   },
   {
     projectName: "Memory Game",
@@ -49,6 +116,16 @@ export const projects: ProjectType[] = [
     projectRepoLink: "https://github.com/fulcain/memory-game",
     description: "یک پروژه تمرینی برای تمرین کردن state در ری اکت.",
     projectType: "normal",
+    isPrivate: false,
+    hasLiveLink: true,
+    technologies: [
+      { name: "SASS", icon: icons.SASS },
+      { name: "JAVASCRIPT", icon: icons.JavaScript },
+      {
+        name: "REACT",
+        icon: icons.React,
+      },
+    ],
   },
   {
     projectName: "Invoker Trainer",
@@ -57,6 +134,20 @@ export const projects: ProjectType[] = [
     projectRepoLink: "https://github.com/fulcain/invoker-trainer",
     description: "یک پروژه تمرینی برای هیروی Invoker در بازی Dota2.",
     projectType: "normal",
+    isPrivate: false,
+    hasLiveLink: true,
+    technologies: [
+      { name: "SASS", icon: icons.SASS },
+      { name: "JAVASCRIPT", icon: icons.JavaScript },
+      {
+        name: "REACT",
+        icon: icons.React,
+      },
+      {
+        name: "MUI",
+        icon: icons.MUI,
+      },
+    ],
   },
 ];
 
